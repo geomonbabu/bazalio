276d74ac654fe5941045fed89f2e9dc430ec3a54
Geomon babu <megeomonbabu@gmail.com>
2025-05-09 22:33:07 +0530

    implement mail feedback

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function sendMail(){
+    let params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    };  
+    emailjs.send("service_mqkmw3p", "template_idrryl8", params).then(alert("Your Message has been send Successfully")); 
+}
